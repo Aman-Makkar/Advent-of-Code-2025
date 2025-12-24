4970af1654fea70c56c093dcccd79bab702fa10f
Aman Makkar <67231395+Aman-Makkar@users.noreply.github.com>
2025-12-24 14:01:52 +0000

    Day 1 part 2

---
+++ day1_part2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include <string.h>
+
+void secret_entrance(){
+    FILE *fptr;
+    fptr = fopen("Input.txt", "r");
+    int starting_position = 50;
+    int result = 0;
+    char str[4168];
+    char str2[4168];
+    while(fscanf(fptr, "%s", str) == 1){
+        strncpy(str2, &str[1], 10);
+        if (str[0] == 'L'){
+            result += abs(starting_position - atoi(str2)) / 100 + (starting_position != 0 && starting_position <= atoi(str2));
+            starting_position = ((starting_position - atoi(str2)) % 100 + 100) % 100;  // Ensure the final number is positive
+        }
+        else{
+            result += (starting_position + atoi(str2)) / 100;
+            starting_position = (starting_position + atoi(str2) + 100) % 100;
+        }
+    }
+    printf("%d", result);
+}
+
+int main(){
+
+    secret_entrance();
+    
+    return 0;
+}
